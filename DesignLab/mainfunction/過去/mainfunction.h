d1c49f76db4c87014ca9ff87103e76d08c721155
Hasegawa Taisei <hasehasehase61@gmail.com>
2023-05-05 20:32:32 +0900

    不必要なファイルの削除

--- DesignLab/mainfunction/過去/mainfunction.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "..\vectorFunc\vectorFunc.h"
-
-myvector::VECTOR LNODEGCOMtoD_L_Pp_comm(myvector::VECTOR VIn);
-
-void getMap(myvector::VECTOR *p_mapData3D, int* mapData3D_MAX, myvector::VECTOR legPosi[6], myvector::VECTOR COM);
-
-void recalMap(myvector::VECTOR *p_mapData3D, int mapData3D_MAX, myvector::VECTOR CenterOfGravityMovement);
