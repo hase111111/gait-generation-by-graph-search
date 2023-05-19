d9a33f316974df13c3adb9bb796b9533a4a34cdc
hase111111 <hasehasehase61@gmail.com>
2023-05-19 21:17:18 +0900

    Change: MapStateを受け取るように変更した

--- DesignLab/MapState.cpp
+++ DesignLab/MapState.cpp
@@ -2,14 +2,61 @@
 
 MapState::MapState(const EMapCreateMode _mode, const int _option, const bool _do_output)
 {
+	//コンストラクタの指定通りにマップを生成する．
+	map_creator::createMap(_mode, _option, m_map_data, _do_output);
+
+	//地形を分ける．
+	makeDevideMap();
 }
 
-MapState::MapState(const MapState& _other)
+int MapState::getPointNumFromDevideMap(const int _x, const int _y) const
 {
-	m_map_data.clear();
+	if (getDevideMapNum(_x, _y) >= MapConst::LP_DIVIDE_NUM * MapConst::LP_DIVIDE_NUM) { return 0; }
 
-	for (const auto &i : _other.m_map_data)
+	return (int)m_devide_map.at(getDevideMapNum(_x, _y)).size();
+}
+
+myvector::SVector MapState::getPosFromDevideMap(const int _x, const int _y, const int _num) const
+{
+	//存在していなければ全て0のベクトルを返す．
+	if (getDevideMapNum(_x, _y) >= MapConst::LP_DIVIDE_NUM * MapConst::LP_DIVIDE_NUM) { return myvector::SVector(0, 0, 0); }
+
+	if (_num < 0 || m_devide_map.at(getDevideMapNum(_x, _y)).size() <= _num ) { return myvector::SVector(0, 0, 0); }
+
+	//存在しているならば値を返す．
+	return m_devide_map.at(getDevideMapNum(_x, _y)).at(_num);
+}
+
+myvector::SVector MapState::getPos(const int _num) const
+{
+	if (_num < 0 || m_map_data.size() <= _num) { return myvector::SVector(0, 0, 0); }
+
+	return m_map_data.at(_num);
+}
+
+void MapState::makeDevideMap()
+{
+	//マップを切り分ける四角形の辺の長さを算出する．
+	const double _lengthX = (MapConst::MAP_X_MAX - MapConst::MAP_X_MIN) / (double)MapConst::LP_DIVIDE_NUM;
+	const double _lengthY = (MapConst::MAP_Y_MAX - MapConst::MAP_Y_MIN) / (double)MapConst::LP_DIVIDE_NUM;
+
+	m_devide_map.resize(MapConst::LP_DIVIDE_NUM * MapConst::LP_DIVIDE_NUM);
+
+	//マップのデータ全てに処理をする．
+	for (const auto &i : m_map_data)
 	{
-		m_map_data.push_back(i);
+		//xy方向のブロック番号をそれぞれ求める
+		int x = (int)((i.x - MapConst::MAP_X_MIN) / _lengthX);
+		int y = (int)((i.y - MapConst::MAP_Y_MIN) / _lengthY);	
+
+		//マップの範囲内にいる時のみ追加する
+		if (0 <= x && x < MapConst::LP_DIVIDE_NUM) 
+		{
+			if (0 <= y && y < MapConst::LP_DIVIDE_NUM)
+			{
+				//値を挿入する
+				m_devide_map.at(getDevideMapNum(x, y)).push_back(i);
+			}
+		}
 	}
 }
--- DesignLab/MapState.h
+++ DesignLab/MapState.h
@@ -10,14 +10,28 @@ public:
 	MapState() = delete;	//デフォルトコンストラクタは消去して，必ず下のコンストラクタでマップ生成をしないと実体を生成できないようにする．
 
 	MapState(const EMapCreateMode _mode, const int _option, const bool _do_output);
-	MapState(const MapState& _other);	//コピーコンストラクタ
+	MapState(const MapState& _other) = delete;	//コピーコンストラクタ
 
+	//正方形状に切り分けられたマップから，脚設置可能点の数を取得する．
+	int getPointNumFromDevideMap(const int _x, const int _y) const;
+
+	//正方形状に切り分けられたマップから，脚設置可能点の座標を取得する
+	myvector::SVector getPosFromDevideMap(const int _x, const int _y, const int _num) const;
+
+	myvector::SVector getPos(const int _num) const;
 
 private:
+
+	// m_map_data をもとに m_devide_mapに値をセットする
+	void makeDevideMap();
 
 	//ロボットがあるくマップ．脚設置可能点の集合で表現ざれる．
 	std::vector<myvector::SVector> m_map_data;
 
 	//マップが存在する領域を正方形に切り分けて，その中に存在する脚設置可能点を集めたもの．
-	std::map< std::pair<int, int>, std::vector<myvector::SVector> > m_devide_map;
+	//要素は https://atcoder.jp/contests/APG4b/tasks/APG4b_t の 「1次元の配列を多次元配列として使う」の要領で並んでいる．
+	std::vector<std::vector<myvector::SVector> > m_devide_map;
+
+	// m_devide_mapでは要素は1次元の配列として並んでいるので，2つの入力から，どの値をシャン称すればよいのかを計算する．
+	inline int getDevideMapNum(const int _x, const int _y) const { return _x * MapConst::LP_DIVIDE_NUM + _y; }
 };
