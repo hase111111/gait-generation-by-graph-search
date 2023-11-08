31a805d81d1a6ed42e570c2bca744db0a517d77b
hase11111 <hasehasehase61@gmail.com>
2023-11-08 20:10:51 +0900

    Vector2の一部にnoexceptを指定

--- DesignLab/designlab_vector2.h
+++ DesignLab/designlab_vector2.h
@@ -48,27 +48,27 @@ namespace designlab
 		//! @brief このベクトルの長さを返す
 		//! @return このベクトルの長さ
 		//! @note sqrtは重いので，長さの2乗を返すlengthSquare()を使うことを推奨
-		float GetLength() const { return std::sqrt(GetSquaredLength()); }
+		float GetLength() const noexcept { return std::sqrt(GetSquaredLength()); }
 
 		//! @brief このベクトルの長さの2乗を返す
 		//! @return このベクトルの長さの2乗
-		constexpr float GetSquaredLength() const { return Dot(*this); }
+		constexpr float GetSquaredLength() const noexcept { return Dot(*this); }
 
 		//! @brief このベクトルとotherの内積を返す
 		//! @param [in] other 他のベクトル
 		//! @return このベクトルとotherの内積
-		constexpr float Dot(const Vector2& other) const { return x * other.x + y * other.y; }
+		constexpr float Dot(const Vector2& other) const noexcept { return x * other.x + y * other.y; }
 
 		//! @brief このベクトルとotherの外積を返す
 		//! @param [in] other 他のベクトル
 		//! @return このベクトルとotherの外積
 		//! @note 2次元なので，外積はスカラー
-		constexpr float Cross(const Vector2& other) const { return x * other.y - y * other.x; }
+		constexpr float Cross(const Vector2& other) const noexcept { return x * other.y - y * other.x; }
 
 		//! @brief このベクトルとotherの距離を返す
 		//! @param [in] other 他のベクトル
 		//! @return このベクトルとotherの距離
-		float GetDistanceFrom(const Vector2& other) const { return (other - *this).GetLength(); }
+		float GetDistanceFrom(const Vector2& other) const noexcept { return (other - *this).GetLength(); }
 
 		//! @brief このベクトルを正規化したベクトルを返す
 		//! @return 正規化されたベクトル
@@ -77,7 +77,7 @@ namespace designlab
 		//! @brief このベクトルが0ならばtrue
 		//! @return このベクトルが0ならばtrue
 		//! @note 誤差を考慮している
-		constexpr bool IsZero() const { return ::designlab::math_util::IsEqual(x, 0.0f) && ::designlab::math_util::IsEqual(y, 0.0f); }
+		constexpr bool IsZero() const noexcept{ return ::designlab::math_util::IsEqual(x, 0.0f) && ::designlab::math_util::IsEqual(y, 0.0f); }
 
 
 		//! @brief このベクトルを文字列にして返す
