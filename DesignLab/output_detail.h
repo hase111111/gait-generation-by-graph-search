8fbab5808e4a3a1438a2749b33e6f3ab538c39e5
Hasegawa Taisei <hasehasehase61@gmail.com>
2023-12-08 02:29:20 +0900

    コメントの修正

--- DesignLab/output_detail.h
+++ DesignLab/output_detail.h
@@ -1,5 +1,5 @@
 //! @file output_detail.h
-//! @brief コマンドラインに文字を出力する際の詳細．
+//! @brief コマンドラインに文字を出力する際の詳細をまとめた列挙体．
 
 #ifndef DESIGNLAB_OUTPUT_DETAIL_H_
 #define DESIGNLAB_OUTPUT_DETAIL_H_
@@ -10,11 +10,11 @@
 //! @details Settingファイルで指定された優先度以上のメッセージのみ出力する．
 enum class OutputDetail
 {
-	kSystem = 0,	//!< システムメッセージ，常に出力する
-	kError,			//!< エラーメッセージ
-	kWarning,		//!< 警告メッセージ，エラーではないが注意が必要なメッセージ
-	kInfo,			//!< 優先度低めの情報
-	kDebug,			//!< デバッグ時のみ出力，一番優先度が低い
+	kSystem = 0,	//!< システムメッセージ，常に出力する．
+	kError,			//!< エラーメッセージ．
+	kWarning,		//!< 警告メッセージ，エラーではないが注意が必要なメッセージ．
+	kInfo,			//!< 優先度低めの情報．
+	kDebug,			//!< デバッグ時のみ出力，一番優先度が低い．
 };
 
 
