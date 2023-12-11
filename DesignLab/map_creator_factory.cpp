e3d35b4538f446d0d0e7c36dbc635a0deb374a46
hase11111 <hasehasehase61@gmail.com>
2023-12-11 14:44:36 +0900

    不必要になったimporterの削除

--- DesignLab/map_creator_factory.cpp
+++ DesignLab/map_creator_factory.cpp
@@ -4,8 +4,8 @@
 
 #include "map_creator_by_csv.h"
 #include "map_creator_for_simulation.h"
-#include "simulation_setting_importer.h"
 #include "toml_file_importer.h"
+#include "toml_file_setupper.h"
 
 
 
@@ -15,13 +15,13 @@ std::unique_ptr<IMapCreator> MapCreatorFactory::Create(const SimulationSettingRe
 
 	if (record.map_create_mode == MapCreateMode::kFromFile)
 	{
-		map_creator = std::make_unique<MapCreatorByCsv>(SimulationSettingImporter::kFilePath + record.map_file_name);
+		map_creator = std::make_unique<MapCreatorByCsv>(TomlFileSetupper::kTomlFileDirPath + record.map_file_name);
 	}
 	else if (record.map_create_mode == MapCreateMode::kForSimulation)
 	{
 		TomlFileImporter<SimulationMapParameter> simulation_map_parameter_importer;
 
-		const SimulationMapParameter simulation_map_parameter = simulation_map_parameter_importer.ImportOrUseDefault(record.simulation_map_param_file_name);
+		const SimulationMapParameter simulation_map_parameter = simulation_map_parameter_importer.ImportOrUseDefault(TomlFileSetupper::kTomlFileDirPath + record.simulation_map_param_file_name);
 
 		map_creator = std::make_unique<MapCreatorForSimulation>(simulation_map_parameter);
 	}
