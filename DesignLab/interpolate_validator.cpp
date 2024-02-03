2be8acf282da6763bf27a973f3c132b7a5fe36ba
Hasegawa Taisei <hasehasehase61@gmail.com>
2024-02-03 14:39:23 +0900

    補間処理のチェックを追加

---
+++ DesignLab/interpolate_validator.cpp
@@ -0,0 +1,53 @@
+﻿
+/// @author    Hasegawa
+/// @copyright © 埼玉大学 設計工学研究室 2024. All right reserved.
+
+#include "interpolate_validator.h"
+
+
+namespace designlab
+{
+
+InterpolateValidator::InterpolateValidator(const std::shared_ptr<const IHexapodCoordinateConverter>& converter,
+                                           const std::shared_ptr<const IHexapodJointCalculator>& calculator) :
+    converter_(converter),
+    calculator_(calculator),
+    interpolated_node_creator_{ converter_ }
+{
+}
+
+
+bool InterpolateValidator::IsValid(const RobotStateNode& current_node, const RobotStateNode& next_node) const
+{
+    // 補完するノード群を生成する．
+    const std::vector<RobotStateNode> interpolated_nodes =
+        interpolated_node_creator_.CreateInterpolatedNode(current_node, next_node);
+
+    // まず，現在のノードが可動範囲内であるか確認する．
+    const auto joint_current = calculator_->CalculateAllJointState(current_node);
+    if (!calculator_->IsValidAllJointState(current_node, joint_current))
+    {
+        return false;
+    }
+
+    // 次のノードが可動範囲内であるか確認する．
+    const auto joint_next = calculator_->CalculateAllJointState(next_node);
+    if (!calculator_->IsValidAllJointState(next_node, joint_next))
+    {
+        return false;
+    }
+
+    // 補完されたノードが可動範囲内であるか確認する．
+    for (const auto& node : interpolated_nodes)
+    {
+        const auto joint = calculator_->CalculateAllJointState(node);
+        if (!calculator_->IsValidAllJointState(node, joint))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+}
---
+++ DesignLab/interpolate_validator.h
@@ -0,0 +1,46 @@
+﻿
+/// @file      interpolate_validator.h
+/// @author    Hasegawa
+/// @copyright © 埼玉大学 設計工学研究室 2024. All right reserved.
+
+#ifndef DESIGNLAB_INTERPOLATE_VALIDATOR_H_
+#define DESIGNLAB_INTERPOLATE_VALIDATOR_H_
+
+#include <memory>
+
+#include "interface_hexapod_coordinate_converter.h"
+#include "interface_hexapod_joint_calculator.h"
+#include "interpolated_node_creator.h"
+#include "robot_state_node.h"
+
+
+namespace designlab
+{
+
+//! @class InterpolateValidator
+//! @brief 補間の妥当性を検証するクラス．
+class InterpolateValidator final
+{
+public:
+    //! @brief コンストラクタ．
+    //! @param converter 座標変換クラス．
+    InterpolateValidator(const std::shared_ptr<const IHexapodCoordinateConverter>& converter,
+                         const std::shared_ptr<const IHexapodJointCalculator>& calculator);
+
+    //! @brief 2つのノード間を矩形軌道で補完し，可動範囲外を通過しないか検証する．
+    //! @param current_node 現在のノード．
+    //! @param next_node 次のノード．
+    //! @retval true 可動範囲外を通過しない．
+    //! @retval false 可動範囲外を通過する．
+    bool IsValid(const RobotStateNode& current_node, const RobotStateNode& next_node) const;
+
+private:
+    const std::shared_ptr<const IHexapodCoordinateConverter> converter_;
+    const std::shared_ptr<const IHexapodJointCalculator> calculator_;
+
+    InterpolatedNodeCreator interpolated_node_creator_;
+};
+
+}  // namespace designlab
+
+#endif  // DESIGNLAB_INTERPOLATE_VALIDATOR_H_
