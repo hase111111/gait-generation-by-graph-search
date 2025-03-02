0f10038e82dd7d17f36e32928b073c8e0aa8ec3f
T.Hasegawa <t.hasegawa.853@ms.saitama-u.ac.jp>
2025-03-02 19:28:00 +0900

    地形に合わせて胴体を回転させるエッジの追加

---
+++ DesignLab/node_creator_builder_ground_conforming_rot.cpp
@@ -0,0 +1,33 @@
+﻿
+//! @file node_creator_builder_ground_conforming_rot.cpp
+
+// Copyright(c) 2023-2025 Design Engineering Laboratory, Saitama University
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+
+
+#include "node_creator_builder_ground_conforming_rot.h"
+
+#include "cassert_define.h"
+
+namespace designlab {
+
+NodeCreatorBuilderGroundConformingRot::NodeCreatorBuilderGroundConformingRot(
+    const std::shared_ptr<const IHexapodCoordinateConverter>& converter_ptr,
+    const std::shared_ptr<const IHexapodStatePresenter>& presenter_ptr,
+    const std::shared_ptr<const IHexapodPostureValidator>& checker_ptr) :
+    converter_ptr_(converter_ptr),
+    presenter_ptr_(presenter_ptr),
+    checker_ptr_(checker_ptr) {
+    assert(converter_ptr_ != nullptr);
+    assert(presenter_ptr_ != nullptr);
+    assert(checker_ptr_ != nullptr);
+}
+
+void NodeCreatorBuilderGroundConformingRot::Build(
+    const DividedMapState& map,
+    std::map<HexapodMove, std::unique_ptr<INodeCreator>>* node_creator) const {
+
+}
+
+}  // namespace designlab
---
+++ DesignLab/node_creator_builder_ground_conforming_rot.h
@@ -0,0 +1,39 @@
+﻿
+//! @file node_creator_builder_ground_conforming_rot.h
+
+// Copyright(c) 2023-2025 Design Engineering Laboratory, Saitama University
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+
+#ifndef DESIGNLAB_NODE_CREATOR_BUILDER_GROUND_CONFORMING_ROT_H_
+#define DESIGNLAB_NODE_CREATOR_BUILDER_GROUND_CONFORMING_ROT_H_
+
+#include <map>
+#include <memory>
+
+#include "interface_node_creator_builder.h"
+
+namespace designlab {
+
+//! @class NodeCreatorBuilderGroundConformingRot
+//! @brief 波東さんが行った処理と同様の方法になるようにノード生成クラスを生成するクラス．
+class NodeCreatorBuilderGroundConformingRot final : public INodeCreatorBuilder {
+public:
+    NodeCreatorBuilderGroundConformingRot(
+      const std::shared_ptr<const IHexapodCoordinateConverter>& converter_ptr,
+      const std::shared_ptr<const IHexapodStatePresenter>& presenter_ptr,
+      const std::shared_ptr<const IHexapodPostureValidator>& checker_ptr);
+
+    void Build(
+      const DividedMapState& map,
+      std::map<HexapodMove, std::unique_ptr<INodeCreator> >* node_creator) const override;
+
+private:
+    const std::shared_ptr<const IHexapodCoordinateConverter> converter_ptr_;
+    const std::shared_ptr<const IHexapodStatePresenter> presenter_ptr_;
+    const std::shared_ptr<const IHexapodPostureValidator> checker_ptr_;
+};
+
+}  // namespace designlab
+
+#endif  // DESIGNLAB_NODE_CREATOR_BUILDER_GROUND_CONFORMING_ROT_H_
