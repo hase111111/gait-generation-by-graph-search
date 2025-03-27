9c43bfcba1a3fd8e2aa7d07f093c97760a786ed7
T.Hasegawa <t.hasegawa.853@ms.saitama-u.ac.jp>
2025-03-27 14:40:59 +0900

    Add: XR-R1のデータの追加

---
+++ DesignLab/xr_r1.cpp
@@ -0,0 +1,17 @@
+﻿
+//! @file phantomx_mk2.cpp
+
+// Copyright(c) 2023-2025 Design Engineering Laboratory, Saitama University
+// Released under the MIT license
+// https://opensource.org/licenses/mit-license.php
+
+#include "xr_r1.h"
+
+#include <numbers>
+
+#include <cmath>
+
+#include "cassert_define.h"
+#include "math_line_segment2.h"
+#include "math_util.h"
+#include "leg_state.h"
