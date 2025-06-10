41e81b2e2b46666bd0f954ad6dd1c569d2bce1e9
T.Hasegawa <t.hasegawa.853@ms.saitama-u.ac.jp>
2025-06-10 12:31:52 +0900

    Update: フォーマットのスタイルをGoogleに更新

--- DesignLab/node_creator_com_move_straight.cpp
+++ DesignLab/node_creator_com_move_straight.cpp
@@ -9,83 +9,86 @@
 
 #include <numbers>
 
-
 namespace designlab {
 
 NodeCreatorComMoveStraight::NodeCreatorComMoveStraight(
-  const DividedMapState& divided_map,
-  const std::shared_ptr<const IHexapodCoordinateConverter>& converter_ptr,
-  const std::shared_ptr<const IHexapodStatePresenter>& presenter_ptr,
-  const std::shared_ptr<const IHexapodPostureValidator>& checker_ptr,
-  HexapodMove next_move) :
-    map_(divided_map),
-    next_move_(next_move),
-    converter_ptr_(converter_ptr),
-    presenter_ptr_(presenter_ptr),
-    checker_ptr_(checker_ptr) {
-    for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
-        const float rad = static_cast<float>(i) * 2.f * std::numbers::pi_v<float> /
-            static_cast<float>(kCandidateDirectionNum) + std::numbers::pi_v<float> / 16;
+    const DividedMapState& divided_map,
+    const std::shared_ptr<const IHexapodCoordinateConverter>& converter_ptr,
+    const std::shared_ptr<const IHexapodStatePresenter>& presenter_ptr,
+    const std::shared_ptr<const IHexapodPostureValidator>& checker_ptr,
+    HexapodMove next_move)
+    : map_(divided_map),
+      next_move_(next_move),
+      converter_ptr_(converter_ptr),
+      presenter_ptr_(presenter_ptr),
+      checker_ptr_(checker_ptr) {
+  for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
+    const float rad = static_cast<float>(i) * 2.f * std::numbers::pi_v<float> /
+                          static_cast<float>(kCandidateDirectionNum) +
+                      std::numbers::pi_v<float> / 16;
 
-        candidate_directions_[i] = Vector3(std::cos(rad), std::sin(rad), 0.0f);
+    candidate_directions_[i] = Vector3(std::cos(rad), std::sin(rad), 0.0f);
 
-        if (std::cos(rad) < 0.0f) {
-            candidate_directions_[i] *= -1.0f;  // x軸方向に負の値を持つ場合は反転する
-        }
+    if (std::cos(rad) < 0.0f) {
+      candidate_directions_[i] *= -1.0f;  // x軸方向に負の値を持つ場合は反転する
     }
+  }
 }
 
-void NodeCreatorComMoveStraight::Create(const RobotStateNode& current_node,
-                                        int current_num,
-                                        std::vector<RobotStateNode>* output_graph) const {
-    RobotStateNode next_node;
+void NodeCreatorComMoveStraight::Create(
+    const RobotStateNode& current_node, int current_num,
+    std::vector<RobotStateNode>* output_graph) const {
+  RobotStateNode next_node;
 
-    std::array<Vector3, kCandidateDirectionNum> candidate_directions_rotated;
+  std::array<Vector3, kCandidateDirectionNum> candidate_directions_rotated;
 
-    for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
-        candidate_directions_rotated[i] = RotateVector3(candidate_directions_[i],
-                                                        current_node.posture);
+  for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
+    candidate_directions_rotated[i] =
+        RotateVector3(candidate_directions_[i], current_node.posture);
+  }
+
+  for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
+    bool is_able = false;
+
+    next_node = current_node;
+    int able_count = 0;
+
+    for (float j = kMoveDistanceStep; j <= kMaxMoveDistance;
+         j += kMoveDistanceStep) {
+      next_node.ChangeGlobalCenterOfMass(
+          current_node.center_of_mass_global_coord +
+              candidate_directions_rotated[i] * j,
+          false);
+
+      // IsLegInRange，IsStable，IsBodyInterfering を確認する．
+      if (checker_ptr_->IsAllLegInRange(next_node.leg_state,
+                                        next_node.leg_pos) &&
+          checker_ptr_->IsStable(next_node.leg_state, next_node.leg_pos) &&
+          !checker_ptr_->IsBodyInterferingWithGround(next_node, map_)) {
+        is_able = true;
+        able_count = static_cast<int>(j);
+      } else {
+        break;
+      }
     }
 
-    for (size_t i = 0; i < kCandidateDirectionNum; ++i) {
-        bool is_able = false;
+    if (is_able) {
+      next_node.ChangeGlobalCenterOfMass(
+          current_node.center_of_mass_global_coord +
+              candidate_directions_rotated[i] * static_cast<float>(able_count),
+          false);
 
-        next_node = current_node;
-        int able_count = 0;
+      next_node.ChangeToNextNode(current_num, next_move_);
 
-        for (float j = kMoveDistanceStep; j <= kMaxMoveDistance; j += kMoveDistanceStep) {
-            next_node.ChangeGlobalCenterOfMass(current_node.center_of_mass_global_coord +
-                                               candidate_directions_rotated[i] * j, false);
+      // discreate_leg_posを更新する．
+      for (int j = 0; j < HexapodConst::kLegNum; j++) {
+        leg_func::ChangeDiscreteLegPos(j, enums::DiscreteLegPos::kCenter,
+                                       &next_node.leg_state);
+      }
 
-            // IsLegInRange，IsStable，IsBodyInterfering を確認する．
-            if (checker_ptr_->IsAllLegInRange(next_node.leg_state, next_node.leg_pos) &&
-              checker_ptr_->IsStable(next_node.leg_state, next_node.leg_pos) &&
-              !checker_ptr_->IsBodyInterferingWithGround(next_node, map_)) {
-                is_able = true;
-                able_count = static_cast<int>(j);
-            }
-            else {
-                break;
-            }
-        }
-
-        if (is_able) {
-            next_node.ChangeGlobalCenterOfMass(current_node.center_of_mass_global_coord +
-                                               candidate_directions_rotated[i] *
-                                               static_cast<float>(able_count), false);
-
-            next_node.ChangeToNextNode(current_num, next_move_);
-
-            // discreate_leg_posを更新する．
-            for (int j = 0; j < HexapodConst::kLegNum; j++) {
-                leg_func::ChangeDiscreteLegPos(j, enums::DiscreteLegPos::kCenter,
-                                               &next_node.leg_state);
-            }
-
-            output_graph->push_back(next_node);
-        }
+      output_graph->push_back(next_node);
     }
+  }
 }
-
 
 }  // namespace designlab
